f779a97034738a12604d41e3eeab4ef5fb19594a
Simon Vetter <simetist@gmail.com>
2024-07-01 23:59:48 +0200

    Add empty Android Studio project

---
+++ app/src/main/java/com/gmail/simetist/stereomikingcalculator/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.gmail.simetist.stereomikingcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gmail.simetist.stereomikingcalculator.ui.theme.StereoMikingCalculatorTheme
+
+
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			StereoMikingCalculatorTheme {
+				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+					Greeting(
+						name = "Android",
+						modifier = Modifier.padding(innerPadding)
+					)
+				}
+			}
+		}
+	}
+}
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+	Text(
+		text = "Hello $name!",
+		modifier = modifier
+	)
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+	StereoMikingCalculatorTheme {
+		Greeting("Android")
+	}
+}
