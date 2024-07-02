54746aa327d298e059215628b3010c74175c7cac
Simon Vetter <simetist@gmail.com>
2024-07-02 09:36:17 +0200

    Replace empty app project with different template

--- app/src/main/java/com/gmail/simetist/stereomikingcalculator/MainActivity.kt
+++ app/src/main/java/com/gmail/simetist/stereomikingcalculator/MainActivity.kt
@@ -1,53 +1,22 @@
 package com.gmail.simetist.stereomikingcalculator
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.gmail.simetist.stereomikingcalculator.ui.theme.StereoMikingCalculatorTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
-		setContent {
-			StereoMikingCalculatorTheme {
-				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-					Greeting(
-						name = "Android",
-						modifier = Modifier.padding(innerPadding)
-					)
-				}
-			}
+		setContentView(R.layout.activity_main)
+		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+			insets
 		}
-	}
-}
-
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
-}
-
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-	StereoMikingCalculatorTheme {
-		Greeting("Android")
 	}
 }
