6e495163af870934cc731e66b121187c7fa2b5e4
Simon Vetter <simetist@gmail.com>
2024-08-25 13:03:28 +0200

    Release v0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
 		applicationId = "com.gmail.simetist.stereophoniccalculator"
 		minSdk = 24
 		targetSdk = 34
-		versionCode = 1
-		versionName = "0.1.0"
+		versionCode = 2
+		versionName = "0.1.1"
 		
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
